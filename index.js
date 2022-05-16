1f76b814884c499503dff92283e508cdc3757042
Patrick Young <patrickyoung281@gmail.com>
2022-05-16 16:08:52 -0400

    commit tree a3d2ebabdf2aa920b75354b840f9b84ee1dc12fb

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Code your solution here
+
+
+function findMatching (array, name) {
+    let answer = array.filter(drivers => drivers.toUpperCase() === name.toUpperCase() )
+    return answer;
+}
+
+function fuzzyMatch (array, name) {
+    let answer = array.filter(drivers => drivers.indexOf(name, 0) === name.indexOf(name, 0))
+    return answer;
+    };
+
+function matchName (array, string) {
+    let answer = array.filter(drivers => drivers.name === string)
+    return answer;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function findMatching (array, name) {
+    const answer = array.filter(matchingName => matchingName.toLowerCase() === name.toLowerCase());
+    return answer;
+};
+
+
+function fuzzyMatch (array, string) {
+    const answer = array.filter(matchingName => matchingName.indexOf(string, 0) === string.indexOf(string, 0));
+    return answer;
+};
+
+
+function matchName (array, string) {
+    const answer = array.filter(townOrName => townOrName.name === string);
+    return answer;
+};
+
+*/
